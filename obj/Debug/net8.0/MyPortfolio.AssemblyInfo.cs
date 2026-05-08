632de7c703a01362dcacb3443d5f7195cecde3fb
Cheng369khmer <cheng369@example.com>
2026-05-08 12:38:13 +0700

    update code profile

--- obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyPortfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe0eff20b047fb088a4a55cbc2fe14f2a2e41da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c83a12af9cbc8b71d54f43e303258b249255971")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
